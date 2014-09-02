f7857c28772d709090a3864de1c42e2da331d6d0
gcorcoran <gcorcoran@my.wctc.edu>
2014-09-02 18:46:47 -0500

    Initial commit

---
+++ src/java/model/RectangleCalculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author Pip
+ */
+public class RectangleCalculator {
+    
+    public String calculateArea(String length, String width){
+        double area = Double.parseDouble(length)*Double.parseDouble(width);
+        return "" + area;
+    }
+    
+}
